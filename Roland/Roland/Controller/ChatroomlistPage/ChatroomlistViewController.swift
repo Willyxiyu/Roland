e79f5feca84843610be07b58d8b3b83a09b519d7
Willyxiyu <Willy.xiyu@gmail.com>
2021-10-22 19:13:04 +0800

    start building the chatroomlist UI

---
+++ Roland/Roland/Controller/ChatroomlistPage/ChatroomlistViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ChatroomlistViewController.swift
+//  
+//
+//  Created by 林希語 on 2021/10/22.
+//
+
+import UIKit
+
+class ChatroomlistViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        self.view.backgroundColor = .blue
+    }
+    
+}
